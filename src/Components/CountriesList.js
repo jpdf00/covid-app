5370c0264fd06f6eb0f47f7d96086ab510854734
jpdf00 <j.pdf00@gmail.com>
2021-07-16 10:53:07 -0300

    Add error handling on the async code

--- src/Components/CountriesList.js
+++ src/Components/CountriesList.js
@@ -1,27 +1,53 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { changeGlobalData, changeCountryData, changeCountry } from '../Actions/index';
 import Earth from './Earth';
 import Country from './Country';
+import '../Assets/Stylesheets/NotFound.css';
 
 const CountriesList = () => {
   const filter = useSelector((state) => state.filter);
   const countries = useSelector((state) => state.countryData);
   const globe = useSelector((state) => state.globalData);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
   const dispatch = useDispatch();
 
   useEffect(() => {
     fetch('https://api.covid19api.com/summary', { method: 'GET', redirect: 'follow' })
-      .then((resp) => (resp.json()))
+      .then((resp) => {
+        if (resp.ok) {
+          return resp.json();
+        }
+        throw resp;
+      })
       .then((resp) => {
         dispatch(changeGlobalData(resp.Global));
         dispatch(changeCountryData(resp.Countries));
+      })
+      .catch(() => {
+        setError(true);
+      })
+      .finally(() => {
+        setLoading(false);
       });
   }, [dispatch]);
 
   const handleCountryChange = (data) => {
     dispatch(changeCountry(data));
   };
+
+  if (loading) {
+    return (
+      <div className="processing" />
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="NotFound" />
+    );
+  }
 
   return (
     <div>
--- src/Components/DayliesList.js
+++ src/Components/DayliesList.js
@@ -1,23 +1,49 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { changeDayData } from '../Actions/index';
 import AllDays from './AllDays';
 import Dayly from './Dayly';
+import '../Assets/Stylesheets/NotFound.css';
 
 const DayliesList = () => {
   const filter = useSelector((state) => state.filter);
   const country = useSelector((state) => state.country);
   const days = useSelector((state) => state.dayData);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
   const dispatch = useDispatch();
   const url = `https://api.covid19api.com/country/${country}`;
 
   useEffect(() => {
     fetch(url, { method: 'GET', redirect: 'follow' })
-      .then((resp) => (resp.json()))
+      .then((resp) => {
+        if (resp.ok) {
+          return resp.json();
+        }
+        throw resp;
+      })
       .then((resp) => {
         dispatch(changeDayData(resp));
+      })
+      .catch(() => {
+        setError(true);
+      })
+      .finally(() => {
+        setLoading(false);
       });
   }, [dispatch]);
+
+  if (loading) {
+    return (
+      <div className="processing" />
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="NotFound" />
+    );
+  }
 
   return (
     <div>
